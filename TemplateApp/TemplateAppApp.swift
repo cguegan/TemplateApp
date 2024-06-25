6bde6b1aee262df1b7f50bc1e4cb2d016b9d4fb9
Christophe <cguegan@gmail.com>
2024-06-25 08:56:27 +0200

    Initial Commit

---
+++ TemplateApp/TemplateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateAppApp.swift
+//  TemplateApp
+//
+//  Created by Christophe Guégan on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TemplateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
